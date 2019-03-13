d9406ccef22a78f0c4325271f89a2ee4dc31dd74
Katherine Ortega <katy.ja2915@gmail.com>
2019-03-12 20:31:52 -0500

    added picture

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import './styles/style.sass';
+import './images/beer_1.jpg';
 
 
 const obj1 = [1, 2, 3];
-const obj2 = [4, 5, 6,7,8];
+const obj2 = [4, 5, 6, 7, 8];
 const merge = [...obj1, ...obj2];
 
 console.log(merge);
